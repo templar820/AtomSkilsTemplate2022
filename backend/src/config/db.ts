85d6efe091f8eed8d13becf381942e9782b1ea61
Кузнецов Иван Дмитриевич <41190483+templar820@users.noreply.github.com>
2022-07-05 20:14:20 +0300

    Создали БД + админку

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,12 +1,8 @@
 import { Sequelize } from 'sequelize';
+import CONSTANT from "./CONSTANT";
 
-console.log('POSTGRES_DB', process.env.POSTGRES_DB);
-console.log('POSTGRES_USER', process.env.POSTGRES_USER);
-console.log('POSTGRES_PASSWORD', process.env.POSTGRES_PASSWORD);
-console.log('POSTGRES_HOST', process.env.POSTGRES_HOST);
-
-export default new Sequelize(process.env.POSTGRES_DB || 'hacktemplate', process.env.POSTGRES_USER || 'user', process.env.POSTGRES_PASSWORD || 'pass', {
+export default new Sequelize(CONSTANT.DATABASE_NAME, CONSTANT.MONGO_USERNAME, CONSTANT.MONGO_PASSWORD, {
   // host: process.env.POSTGRES_HOST || 'localhost',
-  host: process.env.POSTGRES_HOST || '172.17.01',
+  host: CONSTANT.,
   dialect: 'postgres'
 });
--- scripts/mongo-init.js
+++ scripts/mongo-init.js
@@ -1,22 +1,10 @@
-// #!/bin/bash
-//
-// echo "########### CREATE DATABASE MONGO ###########"
-//
-// mongo --eval "
-// db.auth('$MONGO_INITDB_ROOT_USERNAME', '$MONGO_INITDB_ROOT_PASSWORD');
-// db = db.getSiblingDB('$DB_NAME');
-// db.createUser({ user: '$DB_USER', pwd: '$DB_PASSWORD', roles: [{ role: 'readWrite', db: '$DB_NAME' }] });"
+conn = new Mongo();
+db = conn.getDB("as_2022");
 
 
-db.createUser({
-    user: "user",
-    pwd: "pass",
-    roles: [
-        {
-            role: "readWrite",
-            db: "as_2022"
-        }
-    ]
-})
+db.delete_me.createIndex({ "delete_me": 1 }, { unique: false });
+
+
+
 
 
