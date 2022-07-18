178d8bd34d062602252900272681c25090af7fcb
ivanovla <ouromeo@yandex.ru>
2022-07-18 16:34:49 +0300

    changed routes in backend

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
 import { auth } from '../middleware/authMiddleware';
-import baseRouter from './router';
+import orderRoutes from './order';
 import authRouter from './authRouter';
 
 const router = Router();
 
 router.use(authRouter);
 router.use(auth);
-router.use(baseRouter);
+router.use(orderRoutes);
 
 export default router;
