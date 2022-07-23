39bc5e69d82c0d657184d759c1d8bf388ce265c6
templar820 <ttemplar820@gmail.com>
2022-07-23 19:04:51 +0300

    setups scripts

--- scripts/installation-unix.sh
+++ scripts/installation-unix.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 git config --global credential.helper cache
 
-echo "############## SETUP DOCKER ##############"
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-echo "############## 11111111 ##############"
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-echo "############## 22222222 ##############"
-sudo apt update
-sudo apt install docker-ce -y
-echo "############## 33333333 ##############"
-sudo usermod -aG docker ${USER}
-echo "############## 4444444 ##############"
-su - ${USER}
-echo "############## 55555555 ##############"
-sudo usermod -aG docker ${USER}
+#echo "############## SETUP DOCKER ##############"
+#sudo apt install apt-transport-https ca-certificates curl software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#sudo apt update
+#sudo apt install docker-ce -y
+#sudo usermod -aG docker ${USER}
+#su - ${USER}
+#sudo usermod -aG docker ${USER}
 
 
 echo "############## SETUP DOCKER-COMPOSE ##############"
