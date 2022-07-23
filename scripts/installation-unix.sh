6a5d116703bc847d29213eede4675ef7e5560300
templar820 <ttemplar820@gmail.com>
2022-07-23 19:12:24 +0300

    setups scripts

--- scripts/installation-unix.sh
+++ scripts/installation-unix.sh
@@ -1,15 +1,20 @@
-#!/bin/bash
+
+echo "############## UPDATE GIT CREDENTIALS ##############"
+sudo apt update
+sudo apt install git -y
 git config --global credential.helper cache
 
-#echo "############## SETUP DOCKER ##############"
-#sudo apt install apt-transport-https ca-certificates curl software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-#sudo apt update
-#sudo apt install docker-ce -y
-#sudo usermod -aG docker ${USER}
-#su - ${USER}
-#sudo usermod -aG docker ${USER}
+
+
+echo "############## SETUP DOCKER ##############"
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+su - ${USER}
+sudo usermod -aG docker ${USER}
 
 
 echo "############## SETUP DOCKER-COMPOSE ##############"
