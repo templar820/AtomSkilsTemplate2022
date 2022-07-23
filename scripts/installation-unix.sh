7621e1207083944f3b6f955941c75491e76cecdd
templar820 <ttemplar820@gmail.com>
2022-07-23 18:58:24 +0300

    setups scripts

--- scripts/installation-unix.sh
+++ scripts/installation-unix.sh
@@ -4,11 +4,16 @@
 echo "############## SETUP DOCKER ##############"
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "############## 11111111 ##############"
+
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+echo "############## 22222222 ##############"
 sudo apt update
 sudo apt install docker-ce -y
+echo "############## 33333333 ##############"
 sudo usermod -aG docker ${USER}
 su - ${USER}
+echo "############## 4444444 ##############"
 sudo usermod -aG docker ${USER}
 
 
