ca9277a472aa13acc4084b79a0e3ec44e4d6e978
templar820 <ttemplar820@gmail.com>
2022-07-23 18:54:14 +0300

    setups scripts

--- scripts/installation-unix.sh
+++ scripts/installation-unix.sh
@@ -6,7 +6,6 @@ sudo apt install apt-transport-https ca-certificates curl software-properties-co
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 sudo apt update
-apt-cache policy docker-ce
 sudo apt install docker-ce -y
 sudo usermod -aG docker ${USER}
 su - ${USER}
